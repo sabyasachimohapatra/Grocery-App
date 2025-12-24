8ca663a1016727b0646b7d29d58f3d4e95cdb343
sabyasachimohapatra <mohapatrasabyasachi162004@gmail.com>
2025-12-24 19:00:39 +0530

    Grocery app created

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { connectDB } from "./config/connectDB.js";
+import userRoutes from "./routes/user.routes.js";
+import sellerRoutes from "./routes/seller.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import addressRoutes from "./routes/address.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+
+import { connectCloudinary } from "./config/cloudinary.js";
+dotenv.config();
+
+const app = express();
+
+await connectCloudinary();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URI, 
+  credentials: true,               
+}));
+app.use(cookieParser());
+app.use(express.json());
+
+// Api endpoints
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRoutes);
+app.use("/api/seller", sellerRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
+app.use("/api/order", orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
